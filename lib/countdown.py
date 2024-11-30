35cf6805f51e4d3eb38f05e819b78cd4a4f9ad6b
zamzamnn <zamzamnoorahmed73@gmail.com>
2024-11-30 14:33:37 +0300

    countdown midnight-lab assignment done

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,15 @@
-# your code goes here!
+
+import time  # Add this import statement
+
+def countdown(number):
+    while number > 0:
+        print(f"{number} SECOND(S)!")
+        number -= 1
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(number):
+    while number > 0:
+        print(f"{number} SECOND(S)!")
+        time.sleep(1)  # Delay for 1 second
+        number -= 1
+    print("HAPPY NEW YEAR!")
